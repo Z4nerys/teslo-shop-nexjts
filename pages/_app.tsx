ce43090a3a7a21f14c93ea190b944381ebda56d3
Gasty <gmfkratze@gmail.com>
2023-09-22 13:19:34 -0300

    swr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,22 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { CssBaseline, ThemeProvider } from '@mui/material'
+import useSWR, { SWRConfig } from 'swr'
+
 import { lightTheme } from '@/themes'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline/>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SWRConfig
+      value={{
+        //refreshInterval: 500,
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}
+    >
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SWRConfig>
   )
 }
